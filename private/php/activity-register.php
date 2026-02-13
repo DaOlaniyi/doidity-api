42d1ae5c0396249d89cc42c87c11986855532018
Dawood Oosaala Olaniyi <114123801+DaOlaniyi@users.noreply.github.com>
2026-02-13 00:30:00 -0500

    Modify activity register to insert date and status
    Updated SQL insert statement to include date and status fields.

---
+++ private/php/activity-register.php
@@ -0,0 +1,23 @@
+<?php
+// db.php-style connection
+$mysqli = new mysqli("127.0.0.1", "appuser", "Nm3i1=Fk1P@FZkci1O3?I@D", "testdb");
+if ($mysqli->connect_error) {
+    die("DB error");
+}
+
+// insert into people(name, age)
+$stmt = $mysqli->prepare(
+    "INSERT INTO dates-active (date, index, status) VALUES (?, ?, ?)"
+);
+
+$date = date("n/j/Y g:ia", time());
+$status  = "ACTIVE";
+
+
+$stmt->bind_param("si", $date, null, $status);
+
+$stmt->execute();
+
+$stmt->close();
+$mysqli->close();
+?>
